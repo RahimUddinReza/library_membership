e2386872a3b5e0d01ec82db141c90efaef999ba2
RahimUddinReza01 <rahim.uddin.reza@gmail.com>
2024-01-19 21:54:04 +0600

    first push all the files

---
+++ src/main/java/com/reza/library_membership/LibraryMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.reza.library_membership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryMembershipApplication.class, args);
+	}
+
+}
